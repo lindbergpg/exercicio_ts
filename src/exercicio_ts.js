3ecbef517cb711c8078b40aedb836a5ac00bddde
lindbergpg <Lindbergpg@gmail.com>
2023-09-16 19:06:37 -0300

    Funcoes criadas

---
+++ src/exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function funcaoMultiplicacao(a, b) {
+    return a * b;
+}
+console.log(funcaoMultiplicacao(2, 11));
+function funcaoSaudacao(nome) {
+    return `Olá ` + nome;
+}
+console.log(funcaoSaudacao('Lindberg'));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,12 @@
+function funcaoMultiplicacao(a: number , b: number): number {
+    return a * b;
+}
+
+console.log(funcaoMultiplicacao(2,11));
+
+
+function funcaoSaudacao(nome: string): string {
+    return `Olá ` + nome;
+}
+
+console.log(funcaoSaudacao('Lindberg'));
